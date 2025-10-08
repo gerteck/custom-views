802f4c155f5a8ec500f06d9a338975d14b2149ef
gerteck <changerteck@gmail.com>
2025-10-08 13:08:41 +0800

    Release v1.1.0

--- docs/_markbind/plugins/customviews.js
+++ docs/_markbind/plugins/customviews.js
@@ -3,8 +3,9 @@
  * Injects the CustomViews auto-init script into every page.
  * Configuration is loaded from /customviews.config.json
  */
+// '<script src="../../../dist/custom-views.min.js" data-base-url="/customviews"></script>'
 export function getScripts() {
   return [
-    '<script src="../../../dist/custom-views.min.js" data-base-url="/customviews"></script>'
+    '<script src="https://unpkg.com/@customviews-js/customviews@1.1.0/dist/custom-views.min.js" data-base-url="/customviews"></script>'
   ];
 };
